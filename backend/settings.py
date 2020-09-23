543bf39e3e719aa70bcb0ae4dc6a42bfc47a3532
keschnir <fabien.blin@gmail.com>
2020-09-23 14:23:39 +0200

    settings preprod

--- backend/settings.py
+++ backend/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '-#-_u89qpd589wtt3-0*_xp90t9-u)&0%6w7_hmjfix4v5k%ly'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ['DEBUG'] == 'True')
 
-ALLOWED_HOSTS = ['puls-preprod.herokuapp.com']
+ALLOWED_HOSTS = ['puls-back-prod.herokuapp.com']
 
 
 # Application definition
@@ -155,10 +154,14 @@ MEDIA_URL = '/media/'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-
-AWS_S3_FILE_OVERWRITE = False
+AWS_S3_SIGNATURE_VERSION = os.environ.get("AWS_S3_SIGNATURE_VERSION")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = None
+AWS_S3_ADDRESSING_STYLE = "virtual"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 django_heroku.settings(locals())
