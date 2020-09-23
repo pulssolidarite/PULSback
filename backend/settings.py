d55f57d661569f518bc40b49dcc1cbc8cbf66d0f
keschnir <fabien.blin@gmail.com>
2020-09-23 15:26:23 +0200

    settings preprod

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ['DEBUG'] == 'True')
 
-ALLOWED_HOSTS = ['puls-back-prod.herokuapp.com']
+ALLOWED_HOSTS = ['puls-back-preprod.herokuapp.com']
 
 
 # Application definition
