bb1bd54e36b69586db4c5c0e49d17e48342b3a3c
Brahim Sliti <brahim.sliti@visium.ch>
2022-07-11 13:49:11 +0200

    add free mode text field to terminal

--- backend/settings.py
+++ backend/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pulsimpact',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PASSWORD': 'admin'
     }
 }
 
