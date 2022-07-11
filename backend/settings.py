a68ac20b3d3a6420a7d1b146088718b471e5561d
Brahim Sliti <brahim.sliti@visium.ch>
2022-07-11 14:36:50 +0200

    Revert back settings

--- backend/settings.py
+++ backend/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
