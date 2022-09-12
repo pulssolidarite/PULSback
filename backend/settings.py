a32faf54d738d150630faaa675ed2a785b37432b
Fabien <fabien.blin@gmail.com>
2022-09-12 22:02:15 +0200

    wip

--- backend/settings.py
+++ backend/settings.py
@@ -32,7 +32,7 @@ sentry_sdk.init(
 	# django.contrib.auth) you may enable sending PII data.
 	send_default_pii=True,
 	request_bodies="always",
-
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
