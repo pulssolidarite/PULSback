158fc1376c5995b4c7a7bdcb0afa7199e41aa0fa
Fabien <fabien.blin@gmail.com>
2022-09-12 22:00:32 +0200

    wip

--- backend/settings.py
+++ backend/settings.py
@@ -14,27 +14,25 @@ import os
 import django_heroku
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
-if 'SENTRY_DNS' in os.environ:
-	import sentry_sdk
-	from sentry_sdk.integrations.django import DjangoIntegration
+sentry_sdk.init(
+	dsn=os.environ['SENTRY_DNS'],
+	integrations=[
+		DjangoIntegration(),
+	],
 
-	sentry_sdk.init(
-		dsn=os.environ['SENTRY_DNS'],
-		integrations=[
-			DjangoIntegration(),
-		],
+	# Set traces_sample_rate to 1.0 to capture 100%
+	# of transactions for performance monitoring.
+	# We recommend adjusting this value in production.
+	traces_sample_rate=1.0,
 
-		# Set traces_sample_rate to 1.0 to capture 100%
-		# of transactions for performance monitoring.
-		# We recommend adjusting this value in production.
-		traces_sample_rate=1.0,
+	# If you wish to associate users to errors (assuming you are using
+	# django.contrib.auth) you may enable sending PII data.
+	send_default_pii=True,
+	request_bodies="always",
 
-		# If you wish to associate users to errors (assuming you are using
-		# django.contrib.auth) you may enable sending PII data.
-		send_default_pii=True,
-		request_bodies="always",
-	)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
