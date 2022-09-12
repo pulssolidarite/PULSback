9f3590cd26afba1d333487d84bd6eeadadec8294
Fabien <fabien.blin@gmail.com>
2022-09-12 19:48:06 +0200

    add sentry sdk

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,24 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
+sentry_sdk.init(
+    dsn="https://9c3792a5d6434e0fa45c67d547cd0f44@o1407195.ingest.sentry.io/6741670",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
