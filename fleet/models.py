9bc93e46c8930dc31943daa4660944dd6bcdae35
Guillaume Peyssard <guillaume.peyssard@orange.fr>
2022-09-20 17:40:12 +0200

    Screensaver medias and broadcasts models and views

--- fleet/models.py
+++ fleet/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models import Avg, Sum
 import datetime
+from terminal.models import Terminal
 
 class Customer(models.Model):
     login_user = models.OneToOneField("fleet.User", on_delete=models.PROTECT, related_name="login_customer", null=True)
@@ -109,3 +110,52 @@ class DonationStep(models.Model):
 
     class Meta:
         ordering = ['amount']
+
+
+class ScreensaverMedia(models.Model):
+
+    PUBLIC_SCOPE = "public"
+    PRIVATE_SCOPE = "private"
+
+    SCOPE_CHOICES = (
+        (PUBLIC_SCOPE, "Publique"),
+        (PRIVATE_SCOPE, "Privée"),
+    )
+
+    title = models.CharField(max_length=255)
+
+    scope = models.CharField(max_length=7, choices=SCOPE_CHOICES, default=PRIVATE_SCOPE, help_text="Public : Tous les clients ont accès à ce média. Privée : Seuls les clients visés par une diffusion de ce média chez eux ont accès à ce média.")
+
+    # Only the owner can edit or delete this media. The owner might me an admin or a customer
+    owner = models.ForeignKey(User, related_name="owned_screensave_medias", verbose_name="Propriétaire", help_text="Seul le propriétaire de ce média peut l'éditer ou le supprimer", on_delete=models.CASCADE)
+
+    youtube_video_id = models.CharField(max_length=255, verbose_name="Id de la vidéo youtube")
+
+    class Meta:
+        verbose_name = "Média d'écran de veille"
+        verbose_name_plural = "Médias d'écran de veille"
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        """
+        Just a little validation
+        """
+        assert self.scope == self.PRIVATE_SCOPE or (self.scope == self.PUBLIC_SCOPE and self.owner.is_admin_type()), "Public medias can only be owned by admin user"
+        return super().save(*args, **kwargs)    
+
+
+class ScreensaverBroadcast(models.Model):
+    
+    terminal = models.ForeignKey(Terminal, verbose_name="Terminal", on_delete=models.CASCADE, related_name="screensaver_broadcasts")
+    media = models.ForeignKey(ScreensaverMedia, verbose_name="Média", on_delete=models.CASCADE, related_name="screensaver_broadcasts")
+
+    visible = models.BooleanField(verbose_name="Visible", default=False)
+
+    class Meta:
+        verbose_name = "Diffusion d'écran de veille"
+        verbose_name_plural = "Diffusions d'écran de veille"
+
+    def __str__(self):
+        return f"Diffusion de {self.media} chez {self.customer}"
