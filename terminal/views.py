438314b319138c6668478f5b85a3488895b0c29c
Guillaume Peyssard <guillaume.peyssard@orange.fr>
2022-09-24 02:57:55 +0200

    Fix PaymentFiltered view

--- terminal/views.py
+++ terminal/views.py
@@ -169,154 +169,224 @@ class PaymentFiltered(APIView):
     permission_classes = [IsAuthenticated, IsAdminOrCustomerUser] # Only accessible for admin or customer users
 
     def get(self, request, format=None):
-            campaign = self.request.query_params.get('campaign')
-            terminal = self.request.query_params.get('terminal_id')
-            client = self.request.query_params.get('client_id')
-            formule = self.request.query_params.get('formule_')
-            transaction = self.request.query_params.get('transaction_id')
-            game = self.request.query_params.get('game_id')
-            tpe = self.request.query_params.get('tpe')
-            date = self.request.query_params.get('date')
-            date_start = self.request.query_params.get('date_start')
-            date_end = self.request.query_params.get('date_end')
-            try :
-                results_number = int( self.request.query_params.get('results_number'))
-            except ValueError :
-                try :
-                    results_number = float(self.request.query_params.get('results_number'))
-                    results_number = int(results_number)
-                except ValueError :
-                    results_number = 50
-            url_parameters = self.request.query_params.dict().copy()
-            for key, value in self.request.query_params.items():
-                if ( value in [ 'all', ' ','','DD-MM-YYYY H:m:s', 'DD-MM-YYYY'] or key == 'results_number'):
-                    url_parameters.pop(key)
-            if ( "client_id" in url_parameters) :
-                if ( terminal !=  "all" ) :
-                    users_cl = User.objects.filter(customer_id=client)
-                    owned_terminals = Terminal.objects.filter(owner_id__in=users_cl.values_list('id', flat=True))
-                    l1=set(owned_terminals.values_list('id', flat=True))
-                    l2 = {int(url_parameters['terminal_id'])}
-                    common_vals = l1.intersection(l2)
-                    url_parameters.pop('terminal_id')
-                    url_parameters['terminal_id__in'] = common_vals
-                else :
-                    users_cl = User.objects.filter(customer_id=client)
-                    owned_terminals = Terminal.objects.filter(owner_id__in=users_cl.values_list('id', flat=True))
-                    url_parameters['terminal_id__in'] =  owned_terminals.values_list('id', flat=True)
-                url_parameters.pop('client_id')
-            if ('date_start' in url_parameters) :
-               print (url_parameters['date_start'])
-               val = url_parameters['date_start'].replace('T', ' ')
-               converted_date_start =  datetime.datetime.strptime(val, '%d-%m-%Y %H:%M:%S')
-               url_parameters.pop('date_start')
-               url_parameters['date__gte'] = converted_date_start
-            if ( 'date_end' in url_parameters):
-                val = url_parameters['date_end'].replace('T', ' ')
-                converted_date_end = datetime.datetime.strptime(val , '%d-%m-%Y %H:%M:%S')
-                url_parameters.pop('date_end')
-                url_parameters['date__lt'] = converted_date_end
-            if ( "date" in url_parameters):
-                if ( 'date__lt' in url_parameters):
-                    url_parameters.pop('date__lt')
-                if ('date__gte' in url_parameters) :
-                    url_parameters.pop('date__gte')
-                if ( date =="Today" ) :
-                    now = datetime.datetime.now()
-                    today = datetime.datetime(now.year, now.month, now.day)
-                    url_parameters.pop('date')
-                    url_parameters['date__gte'] = today.date()
-                    url_parameters['date__lt'] = today.date() + datetime.timedelta(days=1)
-                elif (  date =="Yesterday") :
-                    now = datetime.datetime.now()
-                    today = datetime.datetime(now.year, now.month, now.day)
-                    url_parameters.pop('date')
-                    url_parameters['date__gte'] = today.date() - datetime.timedelta(days=1)
-                    url_parameters['date__lt'] = today.date()
-                elif (date == "7days"):
-                    some_day_last_week = datetime.datetime.now().date() - datetime.timedelta(days=7)
-                    monday_of_last_week = some_day_last_week - datetime.timedelta(days=(some_day_last_week.isocalendar()[2] - 1))
-                    monday_of_this_week = monday_of_last_week + datetime.timedelta(days=7)
-                    url_parameters.pop('date')
-                    url_parameters['date__gte'] = some_day_last_week
-                    url_parameters['date__lt'] = datetime.date.today() + datetime.timedelta(days=1)
-                elif (date == "CurrentWeek"):
-                    today = datetime.datetime.now()
-                    monday_of_this_week = today.date() - datetime.timedelta(days=today.weekday())
-                    url_parameters.pop('date')
-                    url_parameters['date__gte'] = monday_of_this_week
-                    url_parameters['date__lt'] = monday_of_this_week + datetime.timedelta(days=7)
-                elif (date == "LastWeek"):
-                    some_day_last_week = datetime.now().date() - datetime.timedelta(days=7)
-                    monday_of_last_week = some_day_last_week - datetime.timedelta(days=(some_day_last_week.isocalendar()[2] - 1))
-                    monday_of_this_week = monday_of_last_week + datetime.timedelta(days=7)
-                    print (monday_of_last_week)
-                    print (monday_of_this_week)
-                    url_parameters.pop('date')
-                    url_parameters['date__gte'] = monday_of_last_week
-                    url_parameters['date__lt'] = monday_of_this_week
-                elif (date == "CurrentMonth"):
-                    now = datetime.datetime.now()
-                    start_month = datetime.datetime(now.year, now.month, 1)
-                    date_on_next_month = start_month + datetime.datetime.timedelta(35)
-                    start_next_month = datetime.datetime(date_on_next_month.year, date_on_next_month.month, 1)
-                    last_day_month = start_next_month - datetime.datetime.timedelta(1)
-                    url_parameters.pop('date')
-                    url_parameters['date__gte'] = start_month.date()
-                    url_parameters['date__lt'] = last_day_month.date()
-                elif (date == "LastMonth"):
-                    today = datetime.datetime.date.today()
-                    first = today.replace(day=1)  # first date of current month
-                    end_previous_month = first - datetime.datetime.timedelta(days=1)
-                    start_previous_month = end_previous_month.replace(day=1)
-                    url_parameters.pop('date')
-                    url_parameters['date__gte'] = start_previous_month
-                    url_parameters['date__lt'] = end_previous_month
-                elif (date == "ThisYear"):
-                    now = datetime.datetime.now()
-                    url_parameters.pop('date')
-                    url_parameters['date__gte'] = now.date().replace(day=1, month=1)
-                    url_parameters['date__lt'] = now.date().replace(day=31, month=12)
-                else :
-                    now = datetime.datetime.now()
-                    url_parameters.pop('date')
-                    url_parameters['date__gte'] = now.date().replace(day=1, month=1, year=now.year - 1)
-                    url_parameters['date__lt'] = now.date().replace(day=31, month=12, year=now.year - 1)
-            if ('donation_formula' in url_parameters and  'payment_terminal' in url_parameters):
-                url_parameters.pop('donation_formula')
-                url_parameters.pop('payment_terminal')
-                donation_formula = self.request.query_params.get('donation_formula')
-                tpe = self.request.query_params.get('payment_terminal')
-                terminals = Terminal.objects.filter(donation_formula=donation_formula,payment_terminal=tpe )
-                res_objects = Payment.objects.filter(**url_parameters).filter(terminal_id__in=terminals)
-                TotalResults = Payment.objects.filter(**url_parameters).filter(terminal_id__in=terminals)
-            elif ('donation_formula'  in url_parameters):
-                url_parameters.pop('donation_formula')
-                donation_formula = self.request.query_params.get('donation_formula')
-                terminals = Terminal.objects.filter(donation_formula=donation_formula)
-                res_objects = Payment.objects.filter(**url_parameters).filter(terminal_id__in=terminals)
-                TotalResults = Payment.objects.filter(**url_parameters).filter(terminal_id__in=terminals)
-            elif   ('payment_terminal'  in url_parameters):
-                url_parameters.pop('payment_terminal')
-                tpe = self.request.query_params.get('payment_terminal')
-                terminals = Terminal.objects.filter( payment_terminal=tpe)
-                res_objects = Payment.objects.filter(**url_parameters).filter(terminal_id__in=terminals)
-                TotalResults = Payment.objects.filter(**url_parameters).filter(terminal_id__in=terminals)
-            else :
-                res_objects = Payment.objects.filter(**url_parameters)
-                TotalResults = Payment.objects.filter(**url_parameters)
+        """
+        Possible query params :
+        - campaign_id
+        - terminal_id
+        - client_id
+        - status
+        - game_id
+        - date
+        - date_start
+        - date_end
+        - results_number
+        - payment_terminal
+        - donation_formula
+        """
 
-            res_objects_excluded = res_objects.exclude(status="Skiped").order_by('-date')[:results_number]
-            res_objects = res_objects.order_by('-date')[:results_number]
-            TotalResults = TotalResults.order_by('-date').count()
-            res = PaymentFullSerializer(res_objects, many=True, context={"request": request})
-            amountSum = res_objects_excluded.aggregate(Sum('amount'))['amount__sum']
-            amountAvg =  res_objects_excluded.aggregate(Avg('amount'))['amount__avg']
-            if (amountAvg ): amountAvg = round(amountAvg,2)
-            if (amountSum is None ): amountSum = 0
-            if (amountAvg is None): amountAvg = 0.0
-            total_games = res_objects.count()
-            return Response({ 'payments' :  res.data, 'amountSum': amountSum, 'amountAvg': amountAvg , 'total_games': total_games, 'TotalResults' : TotalResults}, status=status.HTTP_200_OK)
+        # Fetch params
+        
+        campaign_id = self.request.query_params.get('campaign_id')
+        terminal_id = self.request.query_params.get('terminal_id')
+        client_id = self.request.query_params.get('client_id')
+        donation_formula = self.request.query_params.get('donation_formula')
+        status = self.request.query_params.get('status')
+        game_id = self.request.query_params.get('game_id')
+        date = self.request.query_params.get('date')
+        date_start = self.request.query_params.get('date_start')
+        date_end = self.request.query_params.get('date_end')
+        payment_terminal = self.request.query_params.get('payment_terminal')
+        results_number = self.request.query_params.get('results_number')
+
+        # Parse results_number
+        
+        if results_number:
+            results_number = int(results_number)
+        else:
+            results_number = 50
+
+        # Query payments (all payments if logged user is admin, only customer payments if logged user is customer)
+
+        user: User = request.user
+
+        payments = Payment.objects.none()
+
+        if user.is_staff:
+            payments = Payment.objects.all()
+
+        elif user.is_customer_user():
+            payments = Payment.objects.filter(terminal__customer=user.get_customer())
+
+        else:
+            raise PermissionDenied()
+
+        # Filter by terminals
+
+        if terminal_id and terminal_id not in ["all", "", " "]:
+            payments = payments.filter(terminal=terminal_id)
+
+        # Filter by customer
+
+        if client_id and client_id not in ["all", "", " "]:
+            payments = payments.filter(terminal__customer_id=client_id)
+
+        # Filter by campaign
+
+        if campaign_id and campaign_id not in ["all", "", " "]:
+            payments = payments.filter(campaign_id=campaign_id)
+
+        # Filter by game
+
+        if game_id and campaign_id not in ["all", "", " "]:
+            payments = payments.filter(game_id=game_id)
+
+        # Filter by status
+
+        if status and status not in ["all", "", " "]:
+            payments = payments.filter(status=status)
+
+        # Filter by donation_formula
+
+        # TODO
+
+        # Filter by payment_terminal
+
+        # TODO
+
+        # Filter by date_start
+
+        if date_start and date_start not in ['DD-MM-YYYY H:m:s', 'DD-MM-YYYY', "", " "]:
+            formatted_date = date_start.replace('T', ' ')
+            converted_date_start =  datetime.datetime.strptime(formatted_date, '%d-%m-%Y %H:%M:%S')
+            payments = payments.filter(date__gte=converted_date_start)
+
+        # Filter by date_end
+
+        if date_end and date_end not in ['DD-MM-YYYY H:m:s', 'DD-MM-YYYY', "", " "]:
+            formatted_date = date_end.replace('T', ' ')
+            converted_date_end =  datetime.datetime.strptime(formatted_date, '%d-%m-%Y %H:%M:%S')
+            payments = payments.filter(date__lt=converted_date_end)
+
+        # Filter by date (period)
+
+        if date is "Today":
+            today = datetime.datetime.now().date()
+            tomorrow = today + datetime.timedelta(1)
+            today_start = datetime.datetime.combine(today, datetime.time())
+            today_end = datetime.datetime.combine(tomorrow, datetime.time())
+
+            payments = payments.filter(date__gte=today_start, date_lt=today_end)
+
+        elif date is "Yesterday":
+            today = datetime.datetime.now().date()
+            tomorrow = today + datetime.timedelta(1)
+            after_tomorrow = tomorrow + datetime.timedelta(1)
+            tomorrow_start = datetime.datetime.combine(tomorrow, datetime.time())
+            tomorrow_end = datetime.datetime.combine(after_tomorrow, datetime.time())
+
+            payments = payments.filter(date__gte=tomorrow_start, date_lt=tomorrow_end)
+
+        elif date is "7days":
+            today = datetime.datetime.now().date()
+            today_start = datetime.datetime.combine(today, datetime.time())
+
+            in_8_days = today + datetime.timedelta(8)
+            in_7_days_end = datetime.datetime.combine(in_8_days, datetime.time())
+
+            payments = payments.filter(date__gte=today_start, date_lt=in_7_days_end)
+
+        elif date is "CurrentWeek":
+            today = datetime.datetime.now().date()
+            tomorrow = today + datetime.timedelta(1)
+            today_end = datetime.datetime.combine(tomorrow, datetime.time())
+
+            monday_of_this_week = today.date() - datetime.timedelta(days=today.weekday())
+            monday_of_this_week_start = datetime.datetime.combine(monday_of_this_week, datetime.time())
+
+            payments = payments.filter(date__gte=monday_of_this_week_start, date_lt=today_end)
+
+        elif date is "LastWeek":
+            some_day_last_week = datetime.now().date() - datetime.timedelta(days=7)
+            monday_of_last_week = some_day_last_week - datetime.timedelta(days=(some_day_last_week.isocalendar()[2] - 1))
+            monday_of_last_week_start = datetime.datetime.combine(monday_of_last_week, datetime.time())
+            
+            monday_of_this_week = today.date() - datetime.timedelta(days=today.weekday())
+            monday_of_this_week_start = datetime.datetime.combine(monday_of_this_week, datetime.time())
+
+            payments = payments.filter(date__gte=monday_of_last_week_start, date_lt=monday_of_this_week_start)
+
+        elif date is "CurrentMonth":
+            now = datetime.datetime.now()
+            start_month = datetime.datetime(now.year, now.month, 1)
+            date_on_next_month = start_month + datetime.datetime.timedelta(35)
+            start_next_month = datetime.datetime(date_on_next_month.year, date_on_next_month.month, 1)
+
+            payments = payments.filter(date__gte=start_month, date_lt=start_next_month)
+
+        elif date is "LastMonth":
+            today = datetime.datetime.date.today()
+            first = today.replace(day=1)  # first date of current month
+            end_previous_month = first - datetime.datetime.timedelta(days=1)
+            start_previous_month = end_previous_month.replace(day=1)
+
+            now = datetime.datetime.now()
+            start_this_month = datetime.datetime(now.year, now.month, 1)
+
+            payments = payments.filter(date__gte=start_previous_month, date_lt=start_this_month)
+
+        elif date is "ThisYear":
+            now = datetime.datetime.now()
+            first_day_of_this_year = now.date().replace(day=1, month=1)
+            first_day_of_this_year_begining = datetime.datetime.combine(first_day_of_this_year, datetime.time())
+
+            last_day_of_this_year = now.date().replace(day=31, month=12)
+            first_day_of_next_year = last_day_of_this_year + datetime.timedelta(days=1)
+            first_day_of_next_year_begining = datetime.datetime.combine(first_day_of_next_year, datetime.time())
+
+            payments = payments.filter(date__gte=first_day_of_this_year_begining, date_lt=first_day_of_next_year_begining)
+
+        elif date is "LastYear":
+            now = datetime.datetime.now()
+            first_day_of_next_year = now.date().replace(day=1, month=1, year=now.year - 1)
+            first_day_of_next_year_begining = datetime.datetime.combine(first_day_of_next_year, datetime.time())
+
+            first_day_of_this_year = now.date().replace(day=1, month=1)
+            first_day_of_this_year_begining = datetime.datetime.combine(first_day_of_this_year, datetime.time())
+
+            payments = payments.filter(date__gte=first_day_of_next_year_begining, date_lt=first_day_of_this_year_begining)
+
+        # Extract non skiped payments
+
+        not_skiped_payments = payments.exclude(status="Skiped")
+
+        # Count total payments
+
+        total_payment_count = payments.count()
+
+        # Order by date and extract x first ones
+
+        payments = payments.order_by('-date')[:results_number]
+        not_skiped_payments = not_skiped_payments.order_by('-date')[:results_number]
+
+        # Count filtred results
+
+        payment_count = payments.count()
+        not_skiped_payment_count = not_skiped_payments.count()
+
+        # Serialize filtred payments
+
+        payments_serialized = PaymentFullSerializer(payments, many=True, context={"request": request})
+
+        # Count amount
+
+        amountSum = not_skiped_payment_count.aggregate(Sum('amount'))['amount__sum']
+        amountAvg =  not_skiped_payment_count.aggregate(Avg('amount'))['amount__avg']
+
+        if (amountAvg ): amountAvg = round(amountAvg,2)
+        if (amountSum is None ): amountSum = 0
+        if (amountAvg is None): amountAvg = 0.0
+        
+        return Response({ 'payments' :  payments_serialized.data, 'amountSum': amountSum, 'amountAvg': amountAvg , 'total_games': payment_count, 'TotalResults' : total_payment_count}, status=status.HTTP_200_OK)
 
 
 class CSVviewSet(APIView):
