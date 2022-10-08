f4f234a25eb154ebfac34bc44f38209f8508000c
Guillaume Peyssard <guillaume.peyssard@orange.fr>
2022-10-08 18:53:49 +0200

    Partage formula

--- terminal/models.py
+++ terminal/models.py
@@ -21,10 +21,12 @@ class Terminal(models.Model):
     play_timer = models.BigIntegerField(default=10)
     free_mode_text = models.CharField(max_length=250, blank=True, null=True)
     payment_terminal = models.CharField(max_length=250, null=True, blank=True)
-    donation_formula = models.CharField(max_length=250, null=True)
     donation_min_amount = models.IntegerField(default=1)
     donation_default_amount = models.IntegerField(default=1)
     donation_max_amount = models.IntegerField(default=50)
+    donation_formula = models.CharField(max_length=250, null=True) # Classique, Gratuit, Mécénat, Partage
+    donation_share = models.IntegerField(default=50) # How much per cent of the donation go to the owner of the terminal (only if donation_formula == 'Partage')
+
 
     @property
     def visible_screensaver_broadcasts(self):
@@ -110,6 +112,9 @@ class Payment(models.Model):
     status = models.CharField(max_length=255)
     amount = models.FloatField()
     currency = models.CharField(max_length=255)
+    payment_terminal = models.CharField(max_length=250, null=True) # TODO add choices and set non nullable
+    donation_formula = models.CharField(max_length=250, null=True) # Classique, Gratuit, Mécénat, Partage # TODO add choices and set non nullable
+    donation_share = models.IntegerField(default=50) # How much per cent of the donation go to the owner of the terminal (only if donation_formula == 'Partage')
 
     # Save terminal donation formula and payment terminal in payment
     # In case the terminal donation formula or the payment terminal change in the futur,
@@ -129,3 +134,11 @@ class Payment(models.Model):
                 self.payment_terminal = self.terminal.payment_terminal
         
         super(Payment, self).save(*args, **kwargs)
+        
+    @property
+    def amount_donated(self) -> float:
+        if self.donation_formula == "Partage":
+            return self.amount * float(self.donation_share) / 100
+
+        else:
+            return self.amount
