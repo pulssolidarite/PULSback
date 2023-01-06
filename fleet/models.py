ee9a62fdaaa5e9bdefea4c1b0526331ba4d00a23
Guillaume Peyssard <guillaume.peyssard@orange.fr>
2023-01-06 16:02:27 +0900

    Delete user on customer deletion

--- fleet/models.py
+++ fleet/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
 
 from game.models import Game
 
@@ -13,7 +15,7 @@ class User(AbstractUser):
     user can be assigned to a terminal (user.terminal != None),
     or it can be assigned to a customer (user.customer != None),
     or it can be an admin (user.is_staff == True) (note that admin users can be superusers or not),
-    
+
     The pre save validation makes sure that a user object is an admin or is assigned to a customer or a terminal.
     """
 
@@ -22,17 +24,17 @@ class User(AbstractUser):
     # Referenced my Campaign model with related name 'campaigns'
 
     is_staff = models.BooleanField(
-        _('Admin'),
+        _("Admin"),
         default=False,
-        help_text=_('Can this user log into the Admin space and the Django backoffice'),
+        help_text=_("Can this user log into the Admin space and the Django backoffice"),
     )
 
     is_superuser = models.BooleanField(
-        _('Superadmin'),
+        _("Superadmin"),
         default=False,
         help_text=_(
-            'Designates that this admin has all the permissions in the django backoffice without '
-            'explicitly assigning them.'
+            "Designates that this admin has all the permissions in the django backoffice without "
+            "explicitly assigning them."
         ),
     )
 
@@ -44,20 +46,26 @@ class User(AbstractUser):
         Perform some pre save validations
         """
 
-        assert not (self.get_customer() and self.get_terminal()), "User cannot be assigned to a customer and a terminal in the same time. Use different users."
-        assert not (self.is_staff and self.get_terminal()), "Staff member cannot be assigned to a terminal."
-        assert not (self.is_staff and self.get_customer()), "Staff member cannot be assigned to a customer."
+        assert not (
+            self.get_customer() and self.get_terminal()
+        ), "User cannot be assigned to a customer and a terminal in the same time. Use different users."
+        assert not (
+            self.is_staff and self.get_terminal()
+        ), "Staff member cannot be assigned to a terminal."
+        assert not (
+            self.is_staff and self.get_customer()
+        ), "Staff member cannot be assigned to a customer."
 
         super(User, self).save(*args, **kwargs)
 
     def get_terminal(self):
-        if hasattr(self, 'terminal'):
+        if hasattr(self, "terminal"):
             return self.terminal
 
         return None
 
     def get_customer(self):
-        if hasattr(self, 'customer'):
+        if hasattr(self, "customer"):
             return self.customer
 
         return None
@@ -70,10 +78,14 @@ class User(AbstractUser):
 
 
 class Campaign(models.Model):
-    author = models.ForeignKey(User, on_delete=models.PROTECT, null=True, related_name="campaigns")
+    author = models.ForeignKey(
+        User, on_delete=models.PROTECT, null=True, related_name="campaigns"
+    )
     name = models.CharField(max_length=255)
     logo = models.FileField(null=True, blank=True, upload_to="campaigns/logos/")
-    squared_image = models.FileField(blank=True, null=True, upload_to="campaigns/squared_images/")
+    squared_image = models.FileField(
+        blank=True, null=True, upload_to="campaigns/squared_images/"
+    )
     cover = models.FileField(blank=True, null=True, upload_to="campaigns/covers/")
     description = models.TextField()
     goal_amount = models.IntegerField()
@@ -95,7 +107,7 @@ class Campaign(models.Model):
                     old_featured.save()
             except Campaign.DoesNotExist:
                 pass
-            
+
         super(Campaign, self).save(*args, **kwargs)
 
     @property
@@ -104,38 +116,54 @@ class Campaign(models.Model):
 
     @property
     def avg_donation(self):
-        return self.payments.filter(campaign=self.pk, status="Accepted").aggregate(models.Avg('amount'))['amount__avg']
+        return self.payments.filter(campaign=self.pk, status="Accepted").aggregate(
+            models.Avg("amount")
+        )["amount__avg"]
 
     @property
     def total_today(self):
-        return self.payments.filter(campaign=self.pk, status="Accepted", date=datetime.datetime.today()).aggregate(models.Sum('amount'))['amount__sum']
+        return self.payments.filter(
+            campaign=self.pk, status="Accepted", date=datetime.datetime.today()
+        ).aggregate(models.Sum("amount"))["amount__sum"]
 
     @property
     def total_ever(self):
-        return self.payments.filter(campaign=self.pk, status="Accepted").aggregate(models.Sum('amount'))['amount__sum']
+        return self.payments.filter(campaign=self.pk, status="Accepted").aggregate(
+            models.Sum("amount")
+        )["amount__sum"]
 
     @property
     def last_donations(self):
-        return self.payments.filter(campaign=self.pk, status="Accepted").order_by('date')
+        return self.payments.filter(campaign=self.pk, status="Accepted").order_by(
+            "date"
+        )
 
     def __str__(self):
         return self.name
+
 
 class DonationStep(models.Model):
     amount = models.IntegerField(blank=True)
     image = models.FileField(null=True, blank=True, upload_to="campaigns/actions/")
     text = models.TextField(null=True, blank=True)
-    campaign = models.ForeignKey(Campaign, on_delete=models.PROTECT, related_name="donationSteps", null=True)
+    campaign = models.ForeignKey(
+        Campaign, on_delete=models.PROTECT, related_name="donationSteps", null=True
+    )
 
     def __str__(self):
         return self.text
 
     class Meta:
-        ordering = ['amount']
+        ordering = ["amount"]
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="customer", null=True) # TODO assign user to every customers then set this field to null=False
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="customer",
+        null=True,
+    )  # TODO assign user to every customers then set this field to null=False
 
     company = models.CharField(max_length=255)
     representative = models.CharField(max_length=255, null=True)
@@ -152,11 +180,21 @@ class Customer(models.Model):
 
     # The customer can select its own featured campaign for its own terminals
     # This selected campaign will overwrite the campaign that is selected as featured for everyone
-    featured_campaign = models.ForeignKey(Campaign, on_delete=models.SET_NULL, null=True, blank=True)
+    featured_campaign = models.ForeignKey(
+        Campaign, on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     # The customer can select its own featured game for its own terminals
     # This selected game will overwrite the game that is selected as featured for everyone
-    featured_game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True, blank=True)
+    featured_game = models.ForeignKey(
+        Game, on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     def __str__(self):
         return self.company or ""
+
+
+@receiver(post_delete, sender=Customer)
+def post_delete_user(sender, customer: Customer, *args, **kwargs):
+    if customer.user:  # just in case user is not specified
+        customer.user.delete()
