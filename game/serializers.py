03b9310cf0b4b4ddc30a2a35c1f337c51759c358
Guillaume Peyssard <guillaume.peyssard@orange.fr>
2023-06-08 12:11:35 +0200

    Fix

--- game/serializers.py
+++ game/serializers.py
@@ -7,12 +7,14 @@ from .models import *
 class CoreFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CoreFile
-        fields = '__all__'
+        fields = "__all__"
+
 
 class BiosFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = BiosFile
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CoreSerializer(serializers.ModelSerializer):
     file = CoreFileSerializer(many=False, read_only=True)
@@ -21,39 +23,49 @@ class CoreSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Core
-        fields = '__all__'
+        fields = "__all__"
+
 
 class CoreLightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Core
-        fields = '__all__'
+        fields = "__all__"
 
 
 # Serializer pour le model Game
 class GameFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CoreFile
-        fields = '__all__'
+        fields = "__all__"
+
 
 class GameLightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = '__all__'
+        fields = "__all__"
+
 
 class GameSerializer(serializers.ModelSerializer):
     core = CoreSerializer(many=False, read_only=True)
     file = GameFileSerializer(many=False, read_only=True)
-    file2 = GameFileSerializer(many=False, read_only=True)
     nb_terminals = serializers.ReadOnlyField()
+
+    core_id = serializers.PrimaryKeyRelatedField(
+        queryset=Core.objects.all(), source="core", write_only=True
+    )
+
+    file_id = serializers.PrimaryKeyRelatedField(
+        queryset=GameFile.objects.all(), source="file", write_only=True
+    )
 
     class Meta:
         model = Game
-        fields = '__all__'
+        fields = "__all__"
 
     def get_logo_url(self, game):
         if game.logo:
-            if self.context.get('request'):
-                request = self.context.get('request')
+            if self.context.get("request"):
+                request = self.context.get("request")
                 logo_url = game.logo.url
                 return request.build_absolute_uri(logo_url)
             else:
