435c3240e1c8417b14921fd3f0c6db84e1453aeb
Guillaume Peyssard <guillaume.peyssard@orange.fr>
2023-06-08 12:22:20 +0200

    Fix

--- game/serializers.py
+++ game/serializers.py
@@ -1,63 +1,11 @@
-import datetime
 from rest_framework import serializers
-from .models import *
+from .models import Game
 
 
 # Serializer pour le model Core
-class CoreFileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CoreFile
-        fields = "__all__"
-
-
-class BiosFileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BiosFile
-        fields = "__all__"
-
-
-class CoreSerializer(serializers.ModelSerializer):
-    file = CoreFileSerializer(many=False, read_only=True)
-    bios = BiosFileSerializer(many=False, read_only=True)
-    nb_games = serializers.ReadOnlyField()
-
-    class Meta:
-        model = Core
-        fields = "__all__"
-
-
-class CoreLightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Core
-        fields = "__all__"
-
-
-# Serializer pour le model Game
-class GameFileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CoreFile
-        fields = "__all__"
-
-
-class GameLightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Game
-        fields = "__all__"
 
 
 class GameSerializer(serializers.ModelSerializer):
-    core = CoreSerializer(many=False, read_only=True)
-    file = GameFileSerializer(many=False, read_only=True)
-    nb_terminals = serializers.ReadOnlyField()
-
-    core_id = serializers.PrimaryKeyRelatedField(
-        queryset=Core.objects.all(), source="core", write_only=True, required=False
-    )
-
-    file_id = serializers.PrimaryKeyRelatedField(
-        queryset=GameFile.objects.all(), source="file", write_only=True
-    )
-
     class Meta:
         model = Game
         fields = "__all__"
